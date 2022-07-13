057829b4273503024f78af0d36e97ffe0f3ca82d
Dipak Prasad <dipak@thecodebucket.com>
2022-07-13 10:56:26 +0530

    UI design

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,38 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Xcode on 12/07/22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack (alignment: .leading){
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack{
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendess")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .labelStyle(.trailingIcon)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
---
+++ Scrumdinger/MeetingVeiw.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Xcode on 12/07/22.
+//
+
+import SwiftUI
+
+struct MeetingVeiw: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment:.leading) {
+                    Text("Second Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment:.trailing) {
+                    Text("Second Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle().strokeBorder(lineWidth: 24)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action:{}){
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }.padding()
+    }
+}
+
+struct MeetingView_Preview: PreviewProvider {
+    static var previews: some View {
+        MeetingVeiw()
+    }
+}
